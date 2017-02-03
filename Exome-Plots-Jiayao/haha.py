e09f07584f570234f4ef339d781c8ce369384de9
explorerwjy <explorerwjy@gmail.com>
2017-02-03 13:50:09 -0500

    haha

---
+++ Exome-Plots-Jiayao/haha.py
@@ -0,0 +1,43 @@
+
+def doc_info(sample_id, doc_file, flag_file):
+   info = {}
+   with open(flag_file) as f:
+       for line in f:
+           if 'total' in line:
+               if 'rawreads' not in info:
+                   info['rawreads'] = int(line.split()[0])
+           if 'mapped' in line:
+               if 'mappedreads' not in info:
+                   info['mappedreads'] = int(line.split()[0])
+   with open(doc_file) as f:
+       head = f.readline().strip().split()
+       doc_info = dict(zip(head, f.readline().strip().split()))
+       info['targetedbases'] = int(doc_info['total'])
+       info['mean'] = float(doc_info['mean'])
+       info['median'] = float(doc_info['granular_median'])
+       info['DP1'] = float(doc_info['%_bases_above_1'])
+       info['DP10'] = float(doc_info['%_bases_above_10'])
+       info['DP15'] = float(doc_info['%_bases_above_15'])
+   
+   info['Mapped_perc'] = 100.0 * info['mappedreads'] / info['rawreads']
+   info['Reads_mapped_on_target'] = float(info['targetedbases'])  /  info['mappedreads']
+   write_info = [sample_id, info['rawreads'], info['mappedreads'], info['targetedbases'], info['mean'], info['median'],
+   info['DP1'], info['DP10'], info['DP15'], info['Mapped_perc'], info['Reads_mapped_on_target'] ]
+   return write_info
+
+
+
+       
+
+
+
+fw = open('x.txt', 'w')
+write_info = ['sample_id', '#Raw_Reads', '#Mapped_Reads', '#Bases_mapped_on_Target', 'Avg_depth', 'Median_depth', 'D1(%)',  'D10(%)', 'D15(%)', '%Mapped', '%Reads_mapped_on_target']
+fw.write('\t'.join(write_info) + '\n')
+
+sample_id = '1-14798-01'
+doc_file, flag_file = '1-14798-01-004-004_S52_L004_001.bwamem.mkdup.DoC.sample_summary', '1-14798-01-004-004_S52_L004_001.bwamem.flagstat'
+
+write_info = doc_info(sample_id, doc_file, flag_file)
+fw.write('\t'.join(map(str, write_info)) + '\n')
+fw.close()
