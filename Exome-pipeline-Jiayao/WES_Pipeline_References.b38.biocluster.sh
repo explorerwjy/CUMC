21a459594c7d8df548ba2556776a2194b2e1cd79
explorerwjy <explorerwjy@gmail.com>
2017-03-23 10:35:16 -0400

    b38 on biocluster

---
+++ Exome-pipeline-Jiayao/WES_Pipeline_References.b38.biocluster.sh
@@ -0,0 +1,24 @@
+## Resource Directories
+export EXOMPPLN="$HOME/CUMC/Exome-pipeline-Jiayao/" # Directory containing pipeline shell scripts
+export EXOMRES="$HOME/resources" # Directory containing resources/references for pipeline
+
+#jar files  and directories for software
+GATKJAR="$HOME/bin/GenomeAnalysisTK.jar" #Current GATK jar file
+PICARD="$HOME/bin/picard.jar" #directory containing Picard jar files
+SNPEFF="$HOME/bin/snpEff.jar" # Current snpEff jar file
+
+## References
+export BUILD="b38" # shorthand for build
+#export REF="$EXOMRES/reference_genomes/GRCh38/Homo_sapiens_assembly38.fasta" 
+export REF="$EXOMRES/reference_genomes/GRCh38/GRCh38.no_alt_analysis_set.fa" 
+export HAPMAP="$EXOMRES/references/GRCh38/hapmap_3.3.hg38.vcf.gz" # hapmap vcf from GATK
+export INDEL="$EXOMRES/references/GRCh38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz" # Gold standard INDEL reference from GATK
+export TGVCF="$EXOMRES/references/GRCh38/1000G_omni2.5.hg38.vcf.gz" 
+export DBSNP="$EXOMRES/references/GRCh38/dbsnp_146.hg38.vcf.gz" # dbSNP vcf from GATK
+export ONEKG="$EXOMRES/references/GRCh38/1000G_phase1.snps.high_confidence.hg38.vcf.gz" # 1000 genome SNPs vcf
+export ANNOVAR="$HOME/software_pkg/annovar"
+export ANNHDB="/share/archive/yufengshen/ANNOVAR_DATA/humandb" #Location of annovar databases
+
+#Capture Kit Target Files
+export TGTCODES="AgtV2:AgtV4:AgtV5:AgtV5UTR:NbgV2:NbgV3:IllTS:BigTgt:RefSeq:VCRv2"
+
