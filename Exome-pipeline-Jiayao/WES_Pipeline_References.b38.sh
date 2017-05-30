dad5d7e5f411668054459dfa356f1ede7ab54089
explorerwjy <explorerwjy@gmail.com>
2017-05-29 22:07:05 -0400

    update

--- Exome-pipeline-Jiayao/WES_Pipeline_References.b38.sh
+++ Exome-pipeline-Jiayao/WES_Pipeline_References.b38.sh
@@ -11,14 +11,13 @@ SNPEFF="/home/local/users/jw/bin/snpEff.jar" # Current snpEff jar file
 export BUILD="b38" # shorthand for build
 #export REF="$EXOMRES/reference_genomes/GRCh38/Homo_sapiens_assembly38.fasta" 
 export REF="$EXOMRES/reference_genomes/GRCh38/GRCh38.no_alt_analysis_set.fa" 
-export HAPMAP="$EXOMRES/references/GRCh38/hapmap_3.3.hg38.vcf.gz" # hapmap vcf from GATK
-export INDEL="$EXOMRES/references/GRCh38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz" # Gold standard INDEL reference from GATK
-export TGVCF="$EXOMRES/references/GRCh38/1000G_omni2.5.hg38.vcf.gz" 
-export DBSNP="$EXOMRES/references/GRCh38/dbsnp_146.hg38.vcf.gz" # dbSNP vcf from GATK
-export ONEKG="$EXOMRES/references/GRCh38/1000G_phase1.snps.high_confidence.hg38.vcf.gz" # 1000 genome SNPs vcf
+export HAPMAP="$EXOMRES/references/GRCh38/hapmap_3.3.hg38.vcf" # hapmap vcf from GATK
+export INDEL="$EXOMRES/references/GRCh38/Mills_and_1000G_gold_standard.indels.hg38.vcf" # Gold standard INDEL reference from GATK
+export TGVCF="$EXOMRES/references/GRCh38/1000G_omni2.5.hg38.vcf" 
+export DBSNP="$EXOMRES/references/GRCh38/dbsnp_146.hg38.vcf" # dbSNP vcf from GATK
+export ONEKG="$EXOMRES/references/GRCh38/1000G_phase1.snps.high_confidence.hg38.vcf" # 1000 genome SNPs vcf
 export ANNOVAR='/home/local/users/jw/software_packages/annovar'
 export ANNHDB='/home/local/users/jw/software_packages/annovar/humandb' #Location of annovar databases
-export HUMANREF="$EXOMRES/human_g1k_v37.fasta" # human 1000 genome assembly from GATK
 
 
 #Capture Kit Target Files
