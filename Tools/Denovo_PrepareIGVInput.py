2ccb614165524cf69f2d4cbf1f923d1d92520a07
explorerwjy <explorerwjy@gmail.com>
2017-07-25 12:50:05 -0400

    Denovo IGV

--- Tools/Denovo_PrepareIGVInput.py
+++ Tools/Denovo_PrepareIGVInput.py
@@ -2,7 +2,7 @@
 # Author: jywang	explorerwjy@gmail.com
 
 #=========================================================================
-# PSAP_PrepareInput.py
+# DNM_PrepareIGVInput.py
 # Prepare bamout script and variant table for IGV shot
 # Input: Dir contain subdirs of PSAP results, (PSAP.csv, .ped file is need)
 #        TXT file contains (sampleID,BAM) tuple
@@ -19,8 +19,11 @@ import os
 
 ProjectName = re.compile('\w+')
 SampleName = re.compile('(COL-CHUNG_[a-zA-z0-9]+_Diabetes_[\d]+)*.bam')
+SampleName = re.compile('(CARE[a-zA-Z0-9-]+).bam')
+ProjectName = re.compile('\w+')
 EXM_REF = '$HOME/CUMC/Exome-pipeline-Jiayao/WES_Pipeline_References.b37.sh'
 BAMOUT_CMD = '$HOME/CUMC/Exome-pipeline-Jiayao/ExmAdHoc.15b.BamOut.sh'
+IGV_GENERATE_CMD = '$HOME/CUMC/Exome-IGV-Jiayao/Generate_IGV_plots.sh'
 RUN = 'nohup'
 
 def GetOptions():
@@ -39,12 +42,6 @@ def GetOptions():
                         help='Script to run IGV. ')
     parser.add_argument('-p', '--parallel', type=int,
                         default=20, help='Number of Parallel to go. ')
-    parser.add_argument('--popscore', type=float, default=1e-3,
-                        help='popscore cutoff to generate. ')
-    parser.add_argument('--chet', type=bool, default=True,
-                        help='Whether to generate Chet candidates. ')
-    parser.add_argument('--snp', type=bool, default=False,
-                        help='Whether to generate snp candidates ')
     args = parser.parse_args()
     args.dir = os.path.abspath(args.dir)
     print args.dir
@@ -60,7 +57,7 @@ def GetOptions():
         args.script2 = 'run_IGV.sh'
     return args
 
-class Denovo_Prepare_IGV_Input:
+class DNM_PrepareIGVInput:
 	#def __init__(self, Dir, BAMList, VarList1, VarList2, Script1, Script2):
 	def __init(self, args):
 		self.Dir = os.path.abspath(args.dir)
@@ -73,96 +70,67 @@ class Denovo_Prepare_IGV_Input:
 		self.Script2 = open(args.s2 , 'wb')
 
 	def run(self):
-		dirList = [self.Dir + '/' + x for x in os.listdir(self.Dir)]
-		for subdir in dirList:
-			if not os.path.isdir(subdir):
-				continue
-			subList = [ subdir+'/' + x for x in os.listdir(subdir)]
-			self.OnePed(subList)
-		self.FlushBamout()
-		#self.FlushIGV()
-
-	def OnePed(self, subList):
-		PedFil = None
-		PSAPFil = None
-		for _file in subList:
-			if _file.endswith('.ped'):
-				PedFil = _file
-			if _file.endswith('.csv'):
-				PSAPFil = _file
-		if PedFil == None or PSAPFil == None:
-			return None
-		print PedFil.split('/')[-1], PSAPFil.split('/')[-1]
-		self.ReadCSV(PSAPFil, PedFil)
-
-	def ReadCSV(self, PSAPFil, ped):
 		pedigree = Pedigree(ped)
-		Proband = pedigree.GuessProband()
-		Bam = self.bampath.search(Proband)
-		res = []
-		with open(PSAPFil, 'rb') as csvfile:
+		Bams = BamLocation(bam)
+		with open(_csv, 'rb') as csvfile:
 			reader = csv.reader(csvfile)
 			header = reader.next()
-			idx_Chr = header.index("Chrom")
-			idx_Pos = header.index("Pos")
-			idx_Ref = header.index("Ref")
-			idx_Alt = header.index("Alt")
-			idx_Proband = header.index(Proband)
-			idx_DModel = header.index("Dz.Model."+Proband)
-			idx_PopScore = header.index("popScore."+Proband)
 			for row in reader:
-				var = Variant(row, idx_Chr, idx_Pos, idx_Ref, idx_Alt, idx_Proband, idx_DModel, idx_PopScore)
-				if var.needIGV():
-					res.append(var.Out2L())
-					self.VarList2_hand.write("{}\t{},{}\n".format(var.Out2I(), Bam.BamName, Bam.BamOutName))
-			self.VarList1_hand.write("{}\t{}\n".format(Bam.FullPath, '\t'.join(res)))
+				var = Variant(header, row)
+				var.addPed(pedigree)
+				var.addBam(Bams)
+				res.append(var)
+				fout.write(var.OutVar())
+		fout.close()
+		return res
+		self.FlushBamout()
+		self.FlushIGV()
+
+	def LoadCSV(self, _csv, ped, bam, output):
+		pedigree = Pedigree(ped)
+		Bams = BamLocation(bam)
+		res = []
+		fout = open(output,'wb')
+		with open(_csv, 'rb') as csvfile:
+			reader = csv.reader(csvfile)
+			header = reader.next()
+			for row in reader:
+				var = Variant(header, row)
+				var.addPed(pedigree)
+				var.addBam(Bams)
+				res.append(var)
+				fout.write(var.OutVar())
+		fout.close()
 		return res
 
+	def ReduceVarian2Sample(self):
+		res = {}
+		for variant in variants:
+			if variant.Sample not in res:
+				res[variant.Sample] = Sample(variant)
+			else:
+				res[variant.Sample].AddVar(variant)
+		return res.values()
+
 	def FlushBamout(self):
-		fout = self.Script1
-		fout.write('REF={}\n'.format(EXM_REF))
-		fout.write('CMD={}\n'.format(BAMOUT_CMD)) 
-		fout.write('InpFil={}\n'.format(self.VarList1)) 
-		fout.write("NumJob=`wc -l $InpFil|cut -f1 -d ' '`\n\n")
-		fout.write("seq $NumJob| parallel -j 30 --eta $CMD -i $InpFil -a {} -r $REF\n\n")
-		fout.write("find `pwd` -name \"*.bamout.bam\" > Bamout.bam.list")
-		fout.close()
+        fout = self.Script1
+        fout.write('REF={}\n'.format(EXM_REF))
+        fout.write('CMD={}\n'.format(BAMOUT_CMD))
+        fout.write('InpFil={}\n'.format(self.VarList1))
+        fout.write("NumJob=`wc -l $InpFil|cut -f1 -d ' '`\n\n")
+        fout.write("mkdir -p Bamouts;cd Bamouts\n")
+        fout.write(
+            "seq $NumJob| parallel -j {} --eta $CMD -i $InpFil -a {} -r $REF\n\n".format(self.parallel, "{}"))
+        fout.write("cd ../;find `pwd`/Bamouts/ -name \"*.bamout.bam\" > Bamout.bam.list\n")
+        fout.write("cat {} {} > ALL.bam.list\n".format(
+            self.bampath.bamlocationfile, "Bamout.bam.list"))
+        fout.close()
+
 	def FlushIGV(self):
-		fout = self.Script2
-		fout.write(''.format())
-
-class Indv:
-	def __init__(self,FamID, SampleID, FatherID, MotherID, Sex, Affected):
-		self.FamID = FamID
-		self.SampleID = SampleID
-		self.FatherID = FatherID
-		self.MotherID = MotherID
-		self.Sex = Sex
-		self.Affected = Affected
-	def show(self):
-		print '\t'.join([self.FamID,self.SampleID, self.FatherID, self.MotherID, self.Sex, self.Affected])
-
-#Type: Singleton, Trios and complex Fam.
-class Pedigree:
-	def __init__(self, pedfile):
-		fin = open(pedfile, 'rb')
-		self.Indvs = []
-		for l in fin:
-			if l.startswith('#'):
-				continue
-			llist = l.strip().split('\t')
-			FamID, SampleID, FatherID, MotherID, Sex, Affected = llist[:6]
-			indv = Indv(FamID, SampleID, FatherID, MotherID, Sex, Affected)
-			self.Indvs.append(indv)
-		self.FamID = FamID
-	def GuessProband(self):
-		for Indv in self.Indvs:
-			Indv.show()
-			if Indv.Affected == '2':
-				return Indv.SampleID
-		print self.FamID, "Can't find a Proband"
-		exit()
-		return self.Indvs[0].SampleID
+        fout = self.Script2
+        fout.write(
+            '{} -b ALL.bam.list -v {}'.format(IGV_GENERATE_CMD, self.VarList2))
+        fout.close()
 
 class BAM:
 	def __init__(self, fpath):
@@ -171,7 +139,7 @@ class BAM:
 		self.BamOutName = '.'.join(self.BamName.split('.')[:-1]) + ".bamout.bam"
 		self.Sample = SampleName.search(self.BamName).group(1)
 		#self.Sample = self.BamName
-
+ 
 class BamLocation:
 	def __init__(self, bamlocationfile):
 		fin = open(bamlocationfile, 'rb')
@@ -186,49 +154,48 @@ class BamLocation:
 			if sample in v.FullPath:
 				return v
 		return None
+
 class Variant:
-	def __init__(self, row, idx_Chr, idx_Pos, idx_Ref, idx_Alt, idx_Proband, idx_DModel, idx_PopScore):
-		self.Chrom = row[idx_Chr]
-		self.Pos = int(row[idx_Pos])
-		self.Ref = row[idx_Ref]
-		self.Alt = row[idx_Alt].split(',')
-		self.Sample = row[idx_Proband]
-		self.DModel = row[idx_DModel]
-		self.PopScore = row[idx_PopScore]
+	def __init__(self, header, row):
+		self.Chrom = row[header.index('Chrom')]
+		self.Pos = int(row[header.index('Pos')])
+		self.Sample = row[header.index('Sample')]
 		self.start = max(0, self.Pos - 150)
 		self.end = self.Pos + 150
-	# INDELs and Chet with top PopScore needs IGV comfirm.
-    def addPed(self, pedigree):
-        self.Father = pedigree.Probands[self.Sample].Father
-        self.Mother = pedigree.Probands[self.Sample].Mother
-    def addBam(self, bamlocation ):
-    for bam in bamlocation.Bams.values():
-        if self.Sample == bam.Sample:
-            self.SampleBam = bam
-            self.SampleBamout = bam.BamName.rstrip('.bam')+'.bamout.bam'
-        if self.Father == bam.Sample:
-            self.FatherBam = bam
-            self.FatherBamout = bam.BamName.rstrip('.bam')+'.bamout.bam'
-        if self.Mother == bam.Sample:
-            self.MotherBam = bam
-            self.MotherBamout = bam.BamName.rstrip('.bam')+'.bamout.bam'
-	try:
-		for var in [self.SampleBam, self.SampleBamout, self.FatherBam, self.FatherBamout, self.MotherBam, self.MotherBamout ]:
-			if var not in locals():
-				print 'Cant find {} in locals.'.format(var)
-				exit()
-	except:
+
+	def addPed(self, pedigree):
+		self.Father = pedigree.Probands[self.Sample].Father
+		self.Mother = pedigree.Probands[self.Sample].Mother
+
+	def addBam(self, bamlocation):
 		print self.Sample
-    def OutVar(self):
-        return '{}\t{}\t{}\n'.format(self.Chrom, self.Pos, ','.join([self.SampleBam.BamName, self.SampleBamout, self.FatherBam.BamName, self.FatherBamout, self.MotherBam.BamName, self.MotherBamout]))
+		for bam in bamlocation.Bams.values():
+			if self.Sample == bam.Sample:
+				self.SampleBam = bam
+				self.SampleBamout = bam.BamName.rstrip('.bam')+'.bamout.bam'
+			if self.Father == bam.Sample:
+				self.FatherBam = bam
+				self.FatherBamout = bam.BamName.rstrip('.bam')+'.bamout.bam'
+			if self.Mother == bam.Sample:
+				self.MotherBam = bam
+				self.MotherBamout = bam.BamName.rstrip('.bam')+'.bamout.bam'
+		try:
+			for var in [self.SampleBam, self.SampleBamout, self.FatherBam, self.FatherBamout, self.MotherBam, self.MotherBamout ]:
+				if var not in locals():
+					print 'Cant find {} in locals.'.format(var)
+					exit()
+		except:
+			print self.Sample
+	def OutVar(self):
+		return '{}\t{}\t{}\n'.format(self.Chrom, self.Pos, ','.join([self.SampleBam.BamName, self.SampleBamout, self.FatherBam.BamName, self.FatherBamout, self.MotherBam.BamName, self.MotherBamout]))
 	def Out2L(self):
 		return "-L {}:{}-{}".format(self.Chrom, self.start, self.end)
 	def Out2I(self):
 		return "{}\t{}".format(self.Chrom, self.Pos)
 
 def main():
-	Dir, Bam, List1, List2, script1, script2 = GetOptions()
-	instance = Denovo_Prepare_IGV_Input(Dir, Bam, List1, List2, script1, script2)
+	args = GetOptions()
+	instance = DNM_PrepareIGVInput(args)
 	instance.run()
 	return
 
